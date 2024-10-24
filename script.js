df68151f860fb4d03270e1af148b9b946d217bf2
Mihai-sys <mihaicostros7@gmail.com>
2024-10-24 19:44:50 +0200

    finished quiz

--- script.js
+++ script.js
@@ -6,23 +6,27 @@ class Quiz {
         this.answerButtons = document.getElementById("answer-buttons");
         this.feedback = document.getElementById("feedback");
         this.nextButton = document.getElementById("next-button");
+        this.questionIndex = 20;
+        this.nextButton.addEventListener("click", () =>{
+            this.next();
+        });
     }
 
-    init(){
-      const question = this.questions[0];
-      this.question.textContent = question.question;
-      this.answerButtons.innerHTML ="";
-      for (const answer of question.answers){
+    populateQuizAnswer(answer, question){
         const li = document.createElement("li");
         const btn = document.createElement("button");
         btn.className = "btn";
         btn.textContent = answer.text;
         btn.addEventListener("click", () => {
             if(answer.correct){
+                for(const item of this.answerButtons.children){
+                    item.firstChild.classList.remove("wrong");
+                    item.firstChild.disabled = true;
+                }
                 btn.classList.add("correct");
                 this.feedback.textContent = question.explanation;
                 this.feedback.className = "text-correct";
-                this.nextButton.classList.remove("hide");
+                this.nextButton.classList.remove("hide"); 
             }else{
                 btn.classList.add("wrong");
                 this.feedback.textContent = "Raspuns gresit, te rugam sa incerci din nou!";
@@ -31,7 +35,37 @@ class Quiz {
         })
         li.appendChild(btn);
         this.answerButtons.appendChild(li);
+    }
+
+    init(){
+      const question = this.questions[this.questionIndex];
+      this.question.textContent = question.question;
+      this.answerButtons.innerHTML ="";
+      for (const answer of question.answers){
+        this.populateQuizAnswer(answer, question);
       }
+    }
+
+    next(){
+        this.questionIndex ++;
+        this.feedback.innerHTML = "";
+        this.nextButton.classList.add("hide");
+        if(this.questionIndex === this.questions.length){
+            const modalContainer = document.querySelector(".modal-container");
+            modalContainer.classList.remove("hide");
+            const resetButton = document.getElementById("reset-button");
+            resetButton.addEventListener("click", () =>{
+                //var1 - reload la pagina
+                // window.location.reload();
+
+                // var2 - revenire la prima intrebare
+                this.questionIndex = [0];
+                modalContainer.classList.add("hide");
+                this.init();
+            })
+            return;
+        }
+        this.init();
     }
 }
 
